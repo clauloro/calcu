998caa48c5e1e64a1cb894be97dfdcaac2eb2b59
clauloro <114728388+clauloro@users.noreply.github.com>
2024-09-30 21:26:35 +0200

    test

---
+++ calculadora-tdd-01-empezamos/tests/test_.py
@@ -0,0 +1,19 @@
+import unittest
+from src import calculadora 
+
+
+class Testcalculadora(unittest.TestCase):
+    class Testcalculadora(unittest.TestCase):
+        def setUp(self):
+            self.calculadora = calculadora()
+        def test_myfactorial(self):
+            self.assertEqual(self.calculadora.factorial(3), 6)
+
+        def test_factorial_zero(self):
+            self.assertEqual(self.calculadora.factorial(0), 1)
+
+        def test_factorial_negative(self):
+            with self.assertRaises(ValueError):
+                self.calculadora.factorial(-1)
+
+    
--- calculadora-tdd-01-empezamos/tests/test_suma.py
+++
@@ -1,2 +0,0 @@
-from src import calculadora
-import unittest
